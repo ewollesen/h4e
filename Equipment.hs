228dffecae8f5dc97b29b182c6dfb976b1fd24d3
Eric Wollesen <ericw@xmtp.net>
2011-10-27 09:33:19 -0600

    Proper speed modifiers for mail

--- Equipment.hs
+++ Equipment.hs
@@ -19,19 +19,19 @@ heavyArmorTag = tagFactory "Heavy"
 
 plateMail = equipFactory "Plate mail"
             [modFactory "+8 AC bonus (Armor)" "AC" 8,
-             modFactory "-1 Armor penalty" "ArmorPenalty" (-2)]
+             modFactory "-1 Speed penalty (Armor)" "Speed" (-1),
+             modFactory "-2 Armor penalty" "ArmorPenalty" (-2)]
             [heavyArmorTag]
 
 scaleMail = equipFactory "Scale mail"
             [modFactory "+7 AC bonus (Armor)" "AC" 7,
-             modFactory "-1 Armor penalty" "ArmorPenalty" 0]
+             modFactory "-1 Speed penalty (Armor)" "Speed" (-1)]
             [heavyArmorTag]
 
 lightShield = equipFactory "Shield (light)"
               [modFactory "+1 AC bonus (Shield)" "AC" 1,
                modFactory "+1 Reflex bonus (Shield)" "Reflex" 1]
               [lightArmorTag]
-
 
 instance Taggable Equipment where
   tags e = Equipment.tags e
