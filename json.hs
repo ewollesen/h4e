02f86797a228982ef5a52b6127e2a2c9f17765c9
Eric Wollesen <ericw@xmtp.net>
2011-10-28 13:50:16 -0600

    Load a character from a json file and print his character sheet

--- json.hs
+++ json.hs
@@ -13,6 +13,16 @@ import Equipment
 import Modifier
 import Taggable
 import System.IO
+import Pontus
+import Character
+import Weapon
+import Level
+import Race
+import CharacterClass
+import Power
+import Skill
+import Ability
+import CSheet
 
 
 data Foo = Foo { word :: String } deriving (Show)
@@ -27,9 +37,17 @@ getIt c = case c of Right x -> x
                     Left e -> error e
 
 
+$(derive[''Race])
+$(derive[''Weapon])
+$(derive[''Class])
+$(derive[''Level])
 $(derive[''Equipment])
 $(derive[''Modifier])
 $(derive[''Tag])
+$(derive[''Power])
+$(derive[''AbilityName])
+$(derive[''SkillName])
+$(derive[''Character])
 
 --plateMail
 --print $ toJsonString plateMail
@@ -39,3 +57,16 @@ pm' = getIt $ fromJsonString (undefined :: Equipment) pm
 
 dumpIt e f = do
   writeFile f $ toJsonString e
+
+loadIt :: String -> IO ()
+loadIt f = do
+  inh <- openFile f ReadMode
+  jsonData <- hGetContents inh
+  let p' = getIt $ fromJsonString (undefined :: Character) jsonData
+  csheet p'
+  hClose inh
+
+--  x <- readFile f
+--  putStr x
+
+--loadIt e f = getIt $ fromJsonString e (readIt f)
